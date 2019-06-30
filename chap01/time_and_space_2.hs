5609b63e929c63474c0e6f75b459fa25cdd768d5
cutsea110 <cutsea110@gmail.com>
2019-07-01 07:15:36 +0900

    on onepass

--- chap01/time_and_space_2.hs
+++ chap01/time_and_space_2.hs
@@ -3,4 +3,11 @@ module Main where
 goGen u = sum [1..u] + product [1..u]
 goGenShared u = let xs = [1..u] in sum xs + product xs
 
-main = print $ goGenShared 10000
+goGenOnePass u = su + pu
+  where
+    (su,pu) = foldl f (0,1) [1..u]
+    f (s,p) i = let s' = s+i
+                    p' = p*i
+                in s' `seq` p' `seq` (s',p')
+
+main = print $ goGenOnePass 10000
