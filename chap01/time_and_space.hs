132ae60cb39f34e7bff64da7750322217070aeda
cutsea110 <cutsea110@gmail.com>
2019-07-01 05:53:30 +0900

    modify covariant

--- chap01/time_and_space.hs
+++ chap01/time_and_space.hs
@@ -10,7 +10,12 @@ mean v = sum' v / fromIntegral (length v)
 covariance :: [Double] -> [Double] -> Double
 covariance xs ys = sum' (zipWith (\x y -> (x - mean xs) * (y - mean ys)) xs ys) / fromIntegral (length xs)
 
+covariance' :: [Double] -> [Double] -> Double
+covariance' xs ys = let mean_xs = mean xs
+                        mean_ys = mean ys
+                    in sum' (zipWith (\x y -> (x - mean_xs) * (y - mean_ys)) xs ys) / fromIntegral (length xs)
+
 main = do
   let xs = [1, 1.1 .. 500]
       ys = [2, 2.1 .. 501]
-  print $ covariance xs ys
+  print $ covariance' xs ys
