917ea2d99c4f86808f24719415c5f2971b3387a8
cutsea110 <cutsea110@gmail.com>
2019-07-03 07:21:56 +0900

    BitStruct

--- chap02/bitstore.hs
+++ chap02/bitstore.hs
@@ -3,7 +3,7 @@ module Main where
 
 import Data.Array.Unboxed
 import Data.Bits (xor)
-
+{--
 type BitTuple = (Bool, Bool, Bool)
 
 go :: BitTuple -> [Int] -> BitTuple
@@ -14,6 +14,17 @@ test n x = x `mod` n == 0
 
 main :: IO ()
 main = print $ go (False,False,False) [1..1000000]
+--}
 
--- data BitStruct = BitStruct !Bool !Bool !Bool deriving Show
+data BitStruct = BitStruct !Bool !Bool !Bool deriving Show
+
+go :: BitStruct -> [Int] -> BitStruct
+go acc [] = acc
+go (BitStruct two three five) (x:xs) = go (BitStruct (test 2 x `xor` two) (test 3 x `xor` three) (test 5 x `xor` five)) xs
+
+test n x = x `mod` n == 0
+
+main :: IO ()
+main = print $ go (BitStruct False False False) [1..1000000]
+
 -- type BitArray = UArray Int Bool
