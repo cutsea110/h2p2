af8bf3ea26116b7f883fb64c9e32c2126a142052
cutsea110 <cutsea110@gmail.com>
2019-07-02 12:53:24 +0900

    Eureka!

---
+++ chap01/memoization_and_CAFs.hs
@@ -0,0 +1,31 @@
+-- | Eureka!
+-- lift to top-level
+-- >>> :sprint xs
+-- xs = _
+-- >>> fib_mem 0
+-- 1
+-- >>> :sprint xs
+-- xs = 1 : _
+-- >>> fib_mem 1
+-- 1
+-- >>> :sprint xs
+-- xs = 1 : 1 : _
+-- >>> fib_mem 2
+-- 2
+-- >>> :sprint xs
+-- xs = 1 : 1 : 2 : _
+-- >>> fib_mem 10
+-- 89
+-- >>> :sprint xs
+-- xs = 1 : 1 : 2 : 3 : 5 : 8 : 13 : 21 : 34 : 55 : 89 : _
+
+fib_mem :: Int -> Integer
+fib_mem = (xs !!)
+
+xs :: [Integer]
+xs = map fib [0..]
+
+fib :: Int -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib_mem (n-2) + fib_mem (n-1)
