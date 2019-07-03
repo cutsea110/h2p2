133499ece41c2ad9c994dda66d64250c3cf0b965
cutsea110 <cutsea110@gmail.com>
2019-07-04 07:29:16 +0900

    bytestring-perf

---
+++ chap02/bytestring-perf.hs
@@ -0,0 +1,9 @@
+import qualified Data.ByteString as B
+import System.IO (stdin)
+
+go :: Int -> Int -> IO Int
+go 0 s = return $! s
+go n s = do bs <- B.hGet stdin (1024 * 1024)
+            go (n-1) $! B.length bs + s
+
+main = go 2048 0 >>= print
