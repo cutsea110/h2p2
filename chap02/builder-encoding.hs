a289b73aaafa23c432513d3bbe0989910a52cfa2
cutsea110 <cutsea110@gmail.com>
2019-07-05 06:27:24 +0900

    builders for iterative construction

---
+++ chap02/builder-encoding.hs
@@ -0,0 +1,18 @@
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Builder as B
+import Data.Monoid ((<>))
+import System.IO (stdout)
+
+data Tree = Tree !(Int, Tree) !(Int, Tree)
+          | Leaf !ByteString
+
+encodeTree :: Tree -> B.Builder
+encodeTree (Tree (l1, t1) (l2, t2)) = B.charUtf8 '['
+  <> B.intDec l1 <> B.charUtf8 ':' <> encodeTree t1
+  <> B.charUtf8 ','
+  <> B.intDec l2 <> B.charUtf8 ':' <> encodeTree t2
+  <> B.charUtf8 ']'
+encodeTree (Leaf bs) = B.charUtf8 '"' <> B.byteString bs <> B.charUtf8 '"'
+
+main = B.hPutBuilder stdout $ encodeTree $
+  Tree (1, Leaf "one") (2, Tree (3, Leaf "three") (4, Leaf "four"))
