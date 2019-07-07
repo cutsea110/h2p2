cca44323e512a717982793e046e0fb7a4c35ddf3
cutsea110 <cutsea110@gmail.com>
2019-07-08 06:54:24 +0900

    pascal

--- chap02/fib-array-mem.hs
+++ chap02/fib-array-mem.hs
@@ -4,3 +4,11 @@ fib :: Int -> Array Int Integer
 fib n = arr
   where
     arr = listArray (1,n) $ 1:1:[arr!(i-2)+arr!(i-1)|i<-[3..n]]
+
+pascal :: Int -> Array (Int, Int) Integer
+pascal n = arr
+  where
+    arr = array ((1,1), (n,n)) $
+      [ ((i,1), 1) | i <- [1..n]] ++
+      [ ((1,j), 1) | j <- [1..n]] ++
+      [ ((i,j), arr!(i-1, j) + arr!(i, j-1)) | i <- [2..n], j <- [2..n]]
