b2c190faf7305abff1051538a56cbcb60a4d6369
cutsea110 <cutsea110@gmail.com>
2019-07-09 08:32:21 +0900

    mutable references

---
+++ chap02/sum_mutable.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Control.Monad.ST
+import Data.STRef
+
+count_st :: Int -> Int
+count_st n = runST $ do
+  ref <- newSTRef 0
+  let go 0 = readSTRef ref
+      go i = modifySTRef' ref (+ i) >> go (i - 1)
+  go n
+
+count_pure :: Int -> Int
+count_pure n = go n 0
+  where
+    go 0 s = s
+    go i s = go (i - 1) $! (s + i)
+
+main = print "Hello"
