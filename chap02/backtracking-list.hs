0bd127b74d2fa9c13e8525e6103c21a63de913c8
cutsea110 <cutsea110@gmail.com>
2019-07-10 06:36:21 +0900

    naive implementation for Pythagorean triplets

---
+++ chap02/backtracking-list.hs
@@ -0,0 +1,10 @@
+import Control.Monad (guard)
+
+special_pythagorean :: Int -> [(Int, Int, Int)]
+special_pythagorean n = do
+  a <- [1   .. n]
+  b <- [a+1 .. n]
+  c <- [b+1 .. n]
+  guard (a + b + c == n)
+  guard (a ^ 2 + b ^ 2 == c ^ 2)
+  return (a, b, c)
