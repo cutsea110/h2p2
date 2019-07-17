cec26dc3c1f40dfd3b005c80fbd816b8ad8c9f53
cutsea110 <cutsea110@gmail.com>
2019-07-18 08:05:52 +0900

    profiling with SCC

--- chap03/prof-basics.hs
+++ chap03/prof-basics.hs
@@ -2,7 +2,7 @@ sma :: [Double] -> [Double]
 sma (x0:x1:xs) = (x0 + x1) / 2 : sma (x1:xs)
 sma xs         = xs
 
-main = let a = [1..1000000]
-           b = sma a
-           c = sum b
+main = let a = {-# SCC "list-" #-} [1..1000000]
+           b = {-# SCC "sma-" #-} sma a
+           c = {-# SCC "sum-" #-} sum b
            in print c
