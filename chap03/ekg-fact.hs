beca57966d8d4ed4f8eaa891665e71423a327376
cutsea110 <cutsea110@gmail.com>
2019-09-14 10:37:50 +0900

    ekg monitoring

---
+++ chap03/ekg-fact.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Monad
+import System.Remote.Monitoring
+
+main = do
+  forkServer "localhost" 8000
+  forever $ do
+    input <- getLine
+    print $ product [1..read input :: Integer]
