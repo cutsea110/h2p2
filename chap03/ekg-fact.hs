4757eac3a05e40b1bfc08c388a654bc6dc99e618
cutsea110 <cutsea110@gmail.com>
2019-09-14 10:59:27 +0900

    counter

--- chap03/ekg-fact.hs
+++ chap03/ekg-fact.hs
@@ -3,9 +3,15 @@ module Main where
 
 import Control.Monad
 import System.Remote.Monitoring
+import System.Metrics
+import qualified System.Metrics.Counter as Counter
 
+main :: IO ()
 main = do
-  forkServer "localhost" 8000
+  server <- forkServer "localhost" 8000
+  factorials <- createCounter "factorials.count" (serverMetricStore server)
+
   forever $ do
     input <- getLine
     print $ product [1..read input :: Integer]
+    Counter.inc factorials
